65c939dfd85a5bd17cd848586eb3f2f268171f36
Adam Franco <afranco@middlebury.edu>
2016-03-11 01:40:22 -0500

    Autoload now reports parse errors.

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,5 +1,8 @@
 <?php
 
 spl_autoload_register(function ($class) {
-  @include dirname(__FILE__).'/' . str_replace('\\', '/', str_replace('_', '/', $class)) . '.php';
+  $file = dirname(__FILE__).'/' . str_replace('\\', '/', str_replace('_', '/', $class)) . '.php';
+  if (file_exists($file)) {
+    include_once $file;
+  }
 });
